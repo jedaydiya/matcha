4ec777449220088265874dac71785edd123fda31
jedaydiya <joloanjed@gmail.com>
2023-03-16 15:55:10 +0800

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type navbarProps = {};
+
+const navbar: FC<navbarProps> = ({}) => {
+  return (
+    <>
+      <div>navbar</div>
+    </>
+  );
+};
+
+export default navbar;
