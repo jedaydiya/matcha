0514e910dc85249ffdae1e271490ce60630741ab
jedaydiya <joloanjed@gmail.com>
2023-03-16 23:00:19 +0800

    Added Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,84 @@
-import { FC } from "react";
+import React, { useState, FC } from "react";
+import { Link } from "react-router-dom";
 
-type navbarProps = {};
+const NavigationBar: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Navbar: FC<navbarProps> = ({}) => {
+  const toggleNav = () => setIsOpen(!isOpen);
+
   return (
-    <>
-      <div>navbar</div>
-    </>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <a href=""> Matcha Studio</a>
+
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Home
+                </a>
+                <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  About
+                </a>
+                <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleNav}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+            >
+              <svg
+                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+        <div className="px-2 pt-2 pb-3 sm:px-3">
+          <a className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Home
+          </a>
+          <a className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            About
+          </a>
+          <a className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default Navbar;
+export default NavigationBar;
+  
